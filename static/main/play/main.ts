29e16c3a9cedb325bef98b71147a1c7035274dc2
muno9748 <58895791+muno9748@users.noreply.github.com>
2020-06-17 20:28:49 +0900

    Particle and modify collision system

--- static/main/play/main.ts
+++ static/main/play/main.ts
@@ -1,4 +1,6 @@
 //#region enum, interface declation
+const { DisplayObject } = createjs;
+DisplayObject.prototype.zIndex = 0;
 const enum Slot {
     MAINGUN,
     SUBGUN,
@@ -16,12 +18,23 @@ const enum Slot {
     MEDICAL_SODA,
     MEDICAL_PAINKILLER
 }
+const enum ItemType {
+    BULLET_9,
+    BULLET_7_62,
+    BULLET_5_56,
+    BULLET_12GAUGE,
+    MEDICAL_BANDAGE,
+    MEDICAL_HEALTHKIT,
+    MEDICAL_SODA,
+    MEDICAL_PAINKILLER
+}
 const enum ObjectType {
-    BOX
+    BOX,
+    BROKENBOX
 }
 interface Item {
     count: number;
-    name: string;
+    type: ItemType;
     slot: Slot;
     meta?: any;
 }
@@ -30,8 +43,17 @@ interface Player {
     hands: any;
     items: Map<Slot, Item>;
 }
+enum ZIndex {
+    PLAYERBODY = 1000,
+    PLAYERHANDS = 1001,
+    BOX = 901,
+    BROKENBOX = 900,
+    PARTICLE = 9000,
+    BACKGROUND = 0
+}
 // #endregion
 class MainController {
+
     readonly backgroundLineSize: number = 500;
     readonly moveSpeed: number = 8;
 
@@ -48,7 +70,14 @@ class MainController {
         s: boolean;
         d: boolean;
     } = { w: true, a: true, s: true, d: true };
+    private allowedMoveDirectionsReach: {
+        w: boolean;
+        a: boolean;
+        s: boolean;
+        d: boolean;
+    } = { w: true, a: true, s: true, d: true };
     private allowedMoveDirectionsTemp: string[] = [];
+    private allowedMoveDirectionsTempReach: string[] = [];
     private lookingDirection: string = '';
     private collisedBox: any;
     
@@ -62,6 +91,13 @@ class MainController {
             hands: new createjs.Container(),
             items: new Map<Slot, Item>()
         }
+        this.stage.addChild_ = this.stage.addChild;
+        this.stage.addChild = ((c: any) => {
+            this.stage.addChild_(c);
+            this.stage.sortChildren((a: any, b: any) => a.zIndex - b.zIndex);
+        }).bind(this);
+        this.player.body.zIndex = ZIndex.PLAYERBODY;
+        this.player.hands.zIndex = ZIndex.PLAYERHANDS;
     }
 
     public onResize() {
@@ -89,6 +125,7 @@ class MainController {
             a: false,
             d: false
         };
+        this.backgroundLine.zIndex = ZIndex.BACKGROUND;
         this.stage.addChild(this.backgroundLine);
 
         // On Move Controller
@@ -143,56 +180,91 @@ class MainController {
                 );
             }
         }
-        const cCheck = (children: any) => this.collisionCheck({
-            x: this.player.body.x,
-            y: this.player.body.y,
+        const cCheck = (children: any, additionalReach: number = 0) => this.checkCollision({
+            x: window.innerWidth / 2,
+            y: window.innerHeight / 2,
             width: 80,
             height: 80
         }, {
-            x: children.getTransformedBounds().x + 40,
-            y: children.getTransformedBounds().y + 40,
-            width: children.getTransformedBounds().width,
-            height: children.getTransformedBounds().height
+            x: children.x - (children.getTransformedBounds().width / 2) + 40,
+            y: children.y - (children.getTransformedBounds().height / 2) + 40,
+            width: children.image.width * children.scaleX + additionalReach,
+            height: children.image.height * children.scaleY + additionalReach
         });
         let collisions: string[] = [];
+        let _collisions: string[] = [];
         this.stage.children.forEach((boxObj: any, j: number) => {
             if(boxObj.objType != ObjectType.BOX) return;
-            const i: number = boxObj.id_;
-            let c = cCheck(boxObj);
-            if(!c.isCollision) {
-                this.allowedMoveDirections = { 
-                    w: !collisions.includes('w'), 
-                    a: !collisions.includes('a'), 
-                    s: !collisions.includes('s'), 
-                    d: !collisions.includes('d') 
-                };
-                this.allowedMoveDirectionsTemp[i] = ['s', 'w', 'd', 'a'][c.abcd.indexOf(true)];
-            } else {
-                if(this.allowedMoveDirectionsTemp[i] == 'w') {
-                    collisions.push('w');
-                    this.collisedBox = boxObj;
+            let r = cCheck(boxObj);
+            if(r.c) {
+                this.collisedBox = boxObj;
+                if(r.w) {
                     this.allowedMoveDirections.w = false;
-                } else if(this.allowedMoveDirectionsTemp[i] == 's') {
-                    collisions.push('s');
-                    this.collisedBox = boxObj;
-                    this.allowedMoveDirections.s = false;
-                } else if(this.allowedMoveDirectionsTemp[i] == 'a') {
-                    collisions.push('a');
-                    this.collisedBox = boxObj;
+                    collisions.push('w');
+                }
+                if(r.a) {
                     this.allowedMoveDirections.a = false;
-                } else if(this.allowedMoveDirectionsTemp[i] == 'd') {
-                    collisions.push('d');
-                    this.collisedBox = boxObj;
+                    collisions.push('a');
+                }
+                if(r.s) {
+                    this.allowedMoveDirections.s = false;
+                    collisions.push('s');
+                }
+                if(r.d) {
                     this.allowedMoveDirections.d = false;
+                    collisions.push('d');
+                }
+            } else {
+                this.allowedMoveDirections = {
+                    w: !collisions.includes('w'),
+                    a: !collisions.includes('w'),
+                    s: !collisions.includes('w'),
+                    d: !collisions.includes('w')
                 }
             }
+            collisions = collisions.reduce((a: Array<string>, b: string): Array<string> => {
+                if(!a.includes(b)) a.push(b);
+                return a;
+            }, []);
+
+            let _r = cCheck(boxObj);
+            if(_r.c) {
+                this.collisedBox = boxObj;
+                if(_r.w) {
+                    this.allowedMoveDirectionsReach.w = false;
+                    _collisions.push('w');
+                }
+                if(_r.a) {
+                    this.allowedMoveDirectionsReach.a = false;
+                    _collisions.push('a');
+                }
+                if(_r.s) {
+                    this.allowedMoveDirectionsReach.s = false;
+                    _collisions.push('s');
+                }
+                if(_r.d) {
+                    this.allowedMoveDirectionsReach.d = false;
+                    _collisions.push('d');
+                }
+            } else {
+                this.allowedMoveDirectionsReach = {
+                    w: !_collisions.includes('w'),
+                    a: !_collisions.includes('w'),
+                    s: !_collisions.includes('w'),
+                    d: !_collisions.includes('w')
+                }
+            }
+            _collisions = _collisions.reduce((a: Array<string>, b: string): Array<string> => {
+                if(!a.includes(b)) a.push(b);
+                return a;
+            }, []);
         });
     }
 
     private registerSounds() {
-        createjs.Sound.registerSound("/static/assets/sounds/punch.mp3", 'punch');
-        createjs.Sound.registerSound("/static/assets/sounds/boxhit.mp3", 'boxHit');
-        createjs.Sound.registerSound("/static/assets/sounds/boxbreak.mp3", 'boxBreak');
+        createjs.Sound.registerSound('/static/assets/sounds/punch.mp3', 'punch');
+        createjs.Sound.registerSound('/static/assets/sounds/boxhit.mp3', 'boxHit');
+        createjs.Sound.registerSound('/static/assets/sounds/boxbreak.mp3', 'boxBreak');
     }
 
     private onKeyDown(e: KeyboardEvent) {
@@ -227,7 +299,7 @@ class MainController {
         if(this.pressedKeys['d'] && this.allowedMoveDirections['d']) {
             this.playerX += this.moveSpeed;
         }
-        this.stage.children.filter((c: any) => c.isNotFixed).forEach((c: any) => {
+        this.stage.children.filter((c: any) => c.isNotFixed == true).forEach((c: any) => {
             c.x = (-1 * this.playerX) + c.offset.x;
             c.y = (this.playerY) + c.offset.y;
         });
@@ -266,18 +338,137 @@ class MainController {
             });
         this.playSound('punch', .6);
         if(
-            ((!this.allowedMoveDirections['w'] && this.lookingDirection == 'w') ||
-            (!this.allowedMoveDirections['a'] && this.lookingDirection == 'a') ||
-            (!this.allowedMoveDirections['s'] && this.lookingDirection == 's') ||
-            (!this.allowedMoveDirections['d'] && this.lookingDirection == 'd')) && this.collisedBox
+            ((!this.allowedMoveDirectionsReach['w'] && this.lookingDirection == 'w') ||
+            (!this.allowedMoveDirectionsReach['a'] && this.lookingDirection == 'a') ||
+            (!this.allowedMoveDirectionsReach['s'] && this.lookingDirection == 's') ||
+            (!this.allowedMoveDirectionsReach['d'] && this.lookingDirection == 'd')) && this.collisedBox
         ) {
             this.playSound('boxHit', .2);
-            this.collisedBox.damage += 1;
+            this.collisedBox.health -= 1;
             this.collisedBox.scale -= 0.04;
-            if(this.collisedBox.damage == 4) {
+            if(this.collisedBox.health == 1) {
+                const brokenBoxImage = new createjs.Bitmap('/static/assets/images/brokenPlanks.png');
+                brokenBoxImage.offset = {
+                    x: this.collisedBox.offset.x,
+                    y: this.collisedBox.offset.y
+                }
+                brokenBoxImage.visible = false;
+                brokenBoxImage.scale = 0.4;
+                brokenBoxImage.alpha = 0.7;
+                brokenBoxImage.objType = ObjectType.BROKENBOX;
+                brokenBoxImage.isNotFixed = true;
+                brokenBoxImage.zIndex = ZIndex.BROKENBOX;
+                brokenBoxImage.image.onload = () => {
+                    brokenBoxImage.regX = brokenBoxImage.image.width / 2;
+                    brokenBoxImage.regY = brokenBoxImage.image.height / 2;
+                    this.stage.addChild(brokenBoxImage);
+                    setTimeout(() => {
+                        brokenBoxImage.visible = true;
+                    }, 50);
+                }
+            } else if (this.collisedBox.health > 0) {
+                const particle = new createjs.Bitmap('/static/assets/images/particles/brokenPlank.png');
+                particle.scale = 0.3;
+                particle.x = (-1 * this.playerX) + this.collisedBox.offset.x;
+                particle.y = this.playerY + this.collisedBox.offset.y;
+                particle.zIndex = ZIndex.PARTICLE;
+                particle.image.onload = () => {
+                    particle.regX = particle.image.width / 2;
+                    particle.regY = particle.image.height / 2;
+                    const normalizedX: number = Math.floor(Math.random() * 2) * 2 - 1;
+                    const r: number = Math.random() * 360;
+                    const p = {
+                        x: particle.x,
+                        y: particle.y
+                    }
+                    if(['w','s'].includes(this.lookingDirection)) {
+                        createjs.Tween
+                            .get(particle)
+                            .to({ 
+                                x: p.x + (normalizedX * (Math.floor(Math.random() * (50 - 30)) + 30)), 
+                                y: this.lookingDirection == 'w' ? p.y + 100 : p.y - 100, 
+                                rotation: r, 
+                                alpha: 0 
+                            }, 500, createjs.Ease.sineOut)
+                            .call(() => {
+                                this.stage.removeChildAt(this.stage.children.map((c: any) => c.id).indexOf(particle.id));
+                            });
+                        this.stage.addChild(particle);
+                    } else {
+                        createjs.Tween
+                            .get(particle)
+                            .to({ 
+                                x: this.lookingDirection == 'a' ? p.x + 100 : p.x - 100, 
+                                y: p.y + (normalizedX * (Math.floor(Math.random() * (50 - 30)) + 30)), 
+                                rotation: r, 
+                                alpha: 0 
+                            }, 500, createjs.Ease.sineOut)
+                            .call(() => {
+                                this.stage.removeChildAt(this.stage.children.map((c: any) => c.id).indexOf(particle.id));
+                            });
+                        this.stage.addChild(particle);
+                    }
+                }
+            } else if (this.collisedBox.health <= 0) {
                 this.playSound('boxBreak', .2);
-                this.stage.removeChildAt(this.stage.children.map((c: any) => c.id).indexOf(this.collisedBox.id));
-                this.allowedMoveDirections = { w: true, a: true, s: true, d: true };
+                const img = new Image();
+                img.onload = () => {
+                    const { x, y } = this.collisedBox.offset;
+                    for(let i = 0; i < 5; i++) {
+                        const brokenPlankFragment = new createjs.Bitmap(img);
+                        brokenPlankFragment.regX = brokenPlankFragment.image.width / 2;
+                        brokenPlankFragment.regY = brokenPlankFragment.image.height / 2;
+                        brokenPlankFragment.scale = 0.3;
+                        brokenPlankFragment.zIndex = ZIndex.PARTICLE;
+                        brokenPlankFragment.offset = {
+                            x: x,
+                            y: y
+                        }
+                        brokenPlankFragment.isNotFixed = true;
+                        brokenPlankFragment.x = brokenPlankFragment.offset.x + (-1 * this.playerX);
+                        brokenPlankFragment.y = brokenPlankFragment.offset.y + this.playerY;
+                        const r: number = Math.random() * 360;
+                        const [ px, py ] = [ brokenPlankFragment.offset.x, brokenPlankFragment.offset.y ];
+                        const [ tx, ty ] = [
+                            (Math.floor(Math.random() * 2) * 2 - 1 /* normalized */) * (Math.floor(Math.random() * 100)),
+                            (Math.floor(Math.random() * 2) * 2 - 1 /* normalized */) * (Math.floor(Math.random() * 100))
+                        ];
+                        createjs.Tween
+                            .get({
+                                x: px,
+                                y: py,
+                                alpha: 1,
+                                rotation: 0
+                            })
+                            .to({
+                                x: px + tx,
+                                y: py + ty,
+                                alpha: 0.8,
+                                rotation: r
+                            }, 300, createjs.Ease.sineInOut)
+                            .call(() => {
+                                createjs.Tween
+                                    .get(brokenPlankFragment)
+                                    .to({
+                                        alpha: 0
+                                    }, 300, createjs.Ease.sineIn)
+                                    .call(() => {
+                                        this.stage.removeChildAt(this.stage.children.map((c: any) => c.id).indexOf(brokenPlankFragment.id));
+                                    })
+                            })
+                            .on('change', function(this: any) {
+                                brokenPlankFragment.offset.x = this.target.x;
+                                brokenPlankFragment.offset.y = this.target.y;
+                                brokenPlankFragment.alpha = this.target.alpha;
+                                brokenPlankFragment.rotation = this.target.rotation;
+                            })
+                        this.stage.addChild(brokenPlankFragment);
+                    }
+                    this.stage.removeChildAt(this.stage.children.map((c: any) => c.id).indexOf(this.collisedBox.id));
+                    this.allowedMoveDirections = { w: true, a: true, s: true, d: true };
+                    this.collisedBox = null;
+                }
+                img.src = '/static/assets/images/brokenPlankSingle.png';
             }
         }
     }
@@ -297,7 +488,7 @@ class MainController {
         });
     }
 
-    private collisionCheck(rectA: {
+    private checkCollision(rectA: {
         x: number;
         y: number;
         width: number;
@@ -308,29 +499,41 @@ class MainController {
         width: number;
         height: number;
     }) {
+        const [ a, b, c, d, e ]: Array<boolean | string[]> = [
+            rectA.x <= rectB.x + rectB.width,
+            rectA.x + rectA.width >= rectB.x,
+            rectA.y <= rectB.y + rectB.height,
+            rectA.y + rectA.height >= rectB.y,
+            []
+        ]
+        if(a && b && c && d) {
+            if((rectA.y + rectA.height) - this.moveSpeed < rectB.y) (e as string[]).push('s');
+            if(rectA.y > (rectB.y + rectB.height) - this.moveSpeed) (e as string[]).push('w');
+            if((rectA.x + rectA.width) - this.moveSpeed < rectB.x) (e as string[]).push('d');
+            if(rectA.x > (rectB.x + rectB.width) - this.moveSpeed) (e as string[]).push('a');
+        }
+
         return {
-            isCollision: !(rectA.y + rectA.height < rectB.y || rectA.y > rectB.y + rectB.height || rectA.x + rectA.width < rectB.x || rectA.x > rectB.x + rectB.width),
-            abcd: [
-                (rectA.y + rectA.height < rectB.y),
-                (rectA.y > rectB.y + rectB.height),
-                (rectA.x + rectA.width < rectB.x),
-                (rectA.x > rectB.x + rectB.width)
-            ]
+            c: (a && b && c && d) as boolean,
+            w: (e as string[]).includes('w'),
+            a: (e as string[]).includes('a'),
+            s: (e as string[]).includes('s'),
+            d: (e as string[]).includes('d')
         };
     }
 
     private async createBoxes() {
         const boxImage = await this.loadImage('/static/assets/images/box.png');
-        const plankImage = await this.loadImage('/static/assets/images/brokenPlanks.png');
         const createBox = () => {
             const box = new createjs.Bitmap(boxImage);
             box.scale = 0.4;
             box.isNotFixed = true;
             box.regX = boxImage.width / 2;
             box.regY = boxImage.height / 2;
-            box.damage = 0;
+            box.health = 4;
             box.objType = ObjectType.BOX;
             box.offset = {};
+            box.zIndex = ZIndex.BOX;
             return box;
         }
         const generateBoxIDs = () => {
